e2be3a02c148d77c020d405cc27b35a7248628c0
d.nickel1 <d.nickel1@share.epsb.ca>
2022-05-18 15:00:16 -0600

    started on stuff

--- project_list.py
+++ project_list.py
@@ -7,6 +7,7 @@ date-created: 13/5/22
 '''
 
 import sqlite3, sys, pathlib
+from flask import Flask, render_template, request
 
 ### --- Variables -- ###
 
@@ -14,7 +15,52 @@ PROJECT_LIST = "project_list.txt"
 MATERIAL_LIST = "material_list.db"
 
 
-FIRST_RUN = False
+FIRST_RUN = True
 # Either both files exist, or neither do (unless the user does something dumb)
-if (pathlib.Path.cwd() / PROJECT_LIST).exists() and (pathlib.Path.cwd() / MATERIAL_LIST).exists(): FIRST_RUN = True
+if (pathlib.Path.cwd() / PROJECT_LIST).exists() and (pathlib.Path.cwd() / MATERIAL_LIST).exists(): FIRST_RUN = False
 
+### --- Flask --- ###
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Website homepage
+    """
+    return render_template("index.html")
+
+@app.route("/new-project")
+def createProject():
+    global PROJECT_LIST,MATERIAL_LIST
+    CONNECTION = sqlite3.connect(MATERIAL_LIST)
+    CURSOR = CONNECTION.cursor()
+
+    if request.form:
+        pass
+
+
+### --- Inputs --- ###
+
+def createFiles():
+    """Creates needed files. Also I'm not sure why I'm counting this as an input
+    """
+    global PROJECT_LIST,MATERIAL_LIST
+    FILE = open(PROJECT_LIST, "w")
+    FILE.close()
+    FILE = open(MATERIAL_LIST, "w")
+    FILE.close()
+
+### --- Processing --- ###
+
+
+
+### --- Outputs --- ###
+
+
+
+### --- Main Code --- ###
+
+if __name__ == "__main__":
+    if FIRST_RUN:
+        createFiles()
+    app.run(debug=True)
