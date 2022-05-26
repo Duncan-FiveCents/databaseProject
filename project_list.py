11ac8d1da73a29cea66e10369866b75c0e087cc4
d.nickel1 <d.nickel1@share.epsb.ca>
2022-05-26 13:50:54 -0600

    i forfot to pughEISGfhvblrdskjfv

--- project_list.py
+++ project_list.py
@@ -31,12 +31,30 @@ def index():
 
 @app.route("/new-project")
 def createProject():
-    global PROJECT_LIST,MATERIAL_LIST
+    """Form to create a project
+    """
     CONNECTION = sqlite3.connect(MATERIAL_LIST)
     CURSOR = CONNECTION.cursor()
 
-    if request.form:
-        pass
+    if request.form():
+        PROJECT_NAME = request.form.get("title")
+        PROJECT_NAME.replace(" ","-") # Replaces any spaces with hyphens because spaces might break sqlite (I didn't test it)
+        DIFFICULTY = request.form.get("difficulty")
+        TIME = request.form.get("time")
+        CURSOR.execute("""
+            CREATE TABLE ?
+            VALUES(
+                part_name TEXT PRIMARY KEY,
+                pieces TEXT, material TEXT,
+                fin_thick TEXT, fin_width TEXT, fin_lenght TEXT,
+                rough_thick TEXT, rough_width TEXT, rough_length TEXT
+            )
+        ;""")
+        CONNECTION.commit()
+        CONNECTION.close()
+        TEXT = f"(Difficulty: {DIFFICULTY}|Time: {TIME} hours)"
+
+    return render_template("new-project.html")
 
 
 ### --- Inputs --- ###
@@ -50,17 +68,35 @@ def createFiles():
     FILE = open(MATERIAL_LIST, "w")
     FILE.close()
 
+def readFile():
+    """Opens and reads the contents of the instruction file
+
+    Returns:
+        list: data of each line
+    """
+    FILE = open(PROJECT_LIST)
+    DATA = FILE.readlines()
+    FILE.close()
+    return DATA
+
 ### --- Processing --- ###
 
 
 
 ### --- Outputs --- ###
 
+def writeFile(DATA):
+    """Writes data to the given file
 
+    Args:
+        DATA (_type_): _description_
+    """
+    FILE = open(PROJECT_LIST, "w")
+    FILE.writelines(DATA)
+    FILE.close()
 
 ### --- Main Code --- ###
 
 if __name__ == "__main__":
-    if FIRST_RUN:
-        createFiles()
+    if FIRST_RUN: createFiles()
     app.run(debug=True)
